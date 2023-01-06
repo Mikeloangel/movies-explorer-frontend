f540833bd6c640072a019b7788ac3412032efc78
Mikhail Varushichev <mikeloangel@gmail.com>
2023-01-06 14:32:23 +0100

    refactor: FilterCheckbox fancy animation

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -14,26 +14,26 @@ export default function FilterCheckbox({ label, id = 'def', isDisabled, onChange
 
   return (
     <div className='filterbox'>
-      <input
-        type='checkbox'
-        id={`filter_${id}`}
-        name={`filter_${id}`}
-        className='filterbox__check'
-        value={isChecked}
-        onChange={handleCheck}
-        disabled={isDisabled}
-      />
-
       <label
         htmlFor={`filter_${id}`}
         className='filterbox__label'
       >
+        <input
+          type='checkbox'
+          id={`filter_${id}`}
+          name={`filter_${id}`}
+          className='filterbox__check'
+          value={isChecked}
+          onChange={handleCheck}
+          disabled={isDisabled}
+        />
         <div
           className='filterbox__background'
           role='checkbox'
           aria-checked={isChecked}
           aria-labelledby={`filter_${id}`}
         >
+          <div className='filterbox__eyeball'></div>
           <div className='filterbox__eyeball'></div>
         </div>
         {label}
